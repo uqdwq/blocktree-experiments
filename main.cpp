9894f2fb1d736b721107bd9811b81a4ab60f8be0
Daniel <uqdwq@student.kit.edu>
2022-06-20 22:25:34 +0200

    appended CMakeList

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 #include <vector>
 #include <fstream>
 #include <tlx/cmdline_parser.hpp>
-#include "naiveBlocktree/naiveBlocktree.h"
+#include <compressed/PCBlockTree.h>
+#include <compressed/CBlockTree.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include <unordered_set>
+#include "naiveBlockTree/naiveBlockTree.h"
+#include <chrono>
+#include "malloc_count.h"
 
 std::vector<char> read_all_bytes(char const* filename, size_t size) {
     std::ifstream ifs(filename, std::ios::binary|std::ios::ate);
@@ -22,15 +33,20 @@ std::vector<char> read_all_bytes(char const* filename, size_t size) {
 }
 
 int main(int argc, char* argv[]) {
+
     tlx::CmdlineParser cp;
     cp.set_author("Daniel Meyer <uqdwq@student.kit.edu>");
     cp.set_description("This program should run all the expriments for my thesis");
 
     // add a byte size argument which the user can enter like '1gi'
     uint64_t a_size = 0;
-    cp.add_bytes('s', "size", a_size,
+    cp.add_bytes('p', "size", a_size,
                  "Number of bytes to process.");
-
+    int s = 128;
+    int tau = 2;
+    int l = 4;
+    int m = 2;
+    cp.add_int('m', "which mode is to test", m, "");
     // add a required parameter
     std::string a_filename;
     cp.add_param_string("filename", a_filename,
@@ -40,17 +56,84 @@ int main(int argc, char* argv[]) {
     if (!cp.process(argc, argv))
         return -1; // some error occurred and help was always written to user.
 
-    std::cout << "Command line parsed okay." << std::endl;
+    //std::cout << "Command line parsed okay." << std::endl;
 
     // output for debugging
-    cp.print_result();
+    //cp.print_result();
     // std::vector<char> content;
     // content =  read_all_bytes(a_filename.c_str(), a_size);
     // do something useful
-    char name[] = "bla";
-    char *test = "Test"; 
-    std::cout << name << std::endl;
-    std::cout << test << std::endl;
+    std::string input(a_size, ' ');
+    std::ifstream t(a_filename);
+    std::stringstream buffer;
+    t.read(&input[0], a_size);
+    
+    switch (m) {
+        case 0:
+        {
+            auto t01 = std::chrono::high_resolution_clock::now();
+            NBlockTree* bt0 = new NBlockTree(&input,1,2,32);  
+            bt0->build_tree();
+            auto t02 = std::chrono::high_resolution_clock::now();
+            auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t02 - t01);
+            std::cout << input.size() << "," << s << "," << tau << "," << l << "," << ms_int.count() << "," << malloc_count_current() << "," << malloc_count_peak() << std::endl;
+            break;
+        }
+        case 1:
+        {
+            auto t11 = std::chrono::high_resolution_clock::now();
+            PBlockTree* bt1 = new PBlockTree(input, 2, 32);
+            bt1->process_back_pointers();
+            auto t12 = std::chrono::high_resolution_clock::now();
+            auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t12 - t11);
+            std::cout << "PAPER: time: " << ms_int.count() << " curMem: " << malloc_count_current() << " peakMem: " << malloc_count_peak() << std::endl;
+            break;
+        }
+        case 2:
+        {
+            auto t21 = std::chrono::high_resolution_clock::now();
+            NBlockTree* bt2 = new NBlockTree(&input,1,2,32);  
+            bt2->build_tree_fp();
+            auto t22 = std::chrono::high_resolution_clock::now();
+            auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t22 - t21);
+            std::cout << "NAIVE: time: " << ms_int.count() << " curMem: " << malloc_count_current() << " peakMem: " << malloc_count_peak() << std::endl;
+            break;
+        }
+        case 3:
+        {
+            auto t21 = std::chrono::high_resolution_clock::now();
+            NBlockTree* bt2 = new NBlockTree(&input,128,2,32);  
+            bt2->build_tree();
+            auto t22 = std::chrono::high_resolution_clock::now();
+            auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t22 - t21);
+            std::cout << "NAIVE: time: " << ms_int.count() << " curMem: " << malloc_count_current() << " peakMem: " << malloc_count_peak() << std::endl;
+            break;
+            break;
+        }
+        default:
+        {
+            auto td1 = std::chrono::high_resolution_clock::now();
+            NBlockTree* bt3 = new NBlockTree(&input,s,tau,l);  
+            bt3->build_tree();
+            auto td2 = std::chrono::high_resolution_clock::now();
+            auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(td2 - td1);
+            std::cout << "s: " << s << " tau: " << tau << " leaf: " << l << " time: " << ms_int.count() << " curMem: " << malloc_count_current() << " peakMem: " << malloc_count_peak() << std::endl;
+        }
+    }
+    
+ 
+    // for (int c: characters) {
+    //     bt->add_rank_select_support(c);
+    // }
+    // auto tx = std::chrono::high_resolution_clock::now();
+    // ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(tx - t2);
+    // std::cout << ms_int.count() << "ms\n";
+    // std::cout << bt->rank(71,71) << std::endl;
+    
+   
+   
+
+    
     return 0;
 }
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 pushd . > /dev/null
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 while([ -h "${SCRIPT_PATH}" ]); do
@@ -8,6 +8,50 @@ done
 cd "`dirname "${SCRIPT_PATH}"`" > /dev/null
 SCRIPT_PATH="`pwd`";
 popd  > /dev/null
+code="$(date +%Y%m%d%H%M%S)"
+mkdir bench/run$code
+# bench my own with s tau and leaf for different sizes
+sB="1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536"
+Field_Separator=$IFS
+tau="2,4,8,16"
+leaf="4,8,16,32,64"
+IFS=,
 
-"$SCRIPT_PATH"/out/build/BlockTreesExperiments -s 56B "$SCRIPT_PATH"/data/test.txt
+touch bench/run$code/cmpr.data
+sizeD="1,2,4,8,16,32,64,128"
+sizeE="1,2,4,8,16,32,64,128,256"
+#sizeD="1,2,4,8,16,32,64,128"
+factor="MiB"
+for s in $sizeD; do
+    echo "======${s}MB=====" >> bench/run$code/cmpr.data
+    "$SCRIPT_PATH"/out/build/BlockTreesExperiments -p "${s}${factor}" -s 1 -t 2 -l 32 -m 3   "$SCRIPT_PATH"/data/dna.200MB >>  bench/run$code/cmpr.data
+done
+echo "======${SCRIPT_PATH}/data/dna.200MB=====-s 1 -t 2 -l 32" >> bench/run$code/cmpr.data
+for s in $sizeD; do
+    echo "======${s}MB=====" >> bench/run$code/cmpr.data
+    echo "======${s}MB====="
+    "$SCRIPT_PATH"/out/build/BlockTreesExperiments -p "${s}${factor}" -s 1 -t 2 -l 32 -m 1   "$SCRIPT_PATH"/data/dna.200MB >>  bench/run$code/cmpr.data
+    "$SCRIPT_PATH"/out/build/BlockTreesExperiments -p "${s}${factor}" -s 1 -t 2 -l 32 -m 2   "$SCRIPT_PATH"/data/dna.200MB >>  bench/run$code/cmpr.data
+done
+echo "======${SCRIPT_PATH}/data/english.1024MB=====-s 1 -t 2 -l 32" >> bench/run$code/cmpr.data
 
+#sizeE="1,2,4,8"
+for s in $sizeE; do
+    echo "======${s}MB=====" >> bench/run$code/cmpr.data
+    "$SCRIPT_PATH"/out/build/BlockTreesExperiments -p "${s}${factor}" -s 1 -t 2 -l 32 -m 1   "$SCRIPT_PATH"/data/english.1024MB >>  bench/run$code/cmpr.data
+    "$SCRIPT_PATH"/out/build/BlockTreesExperiments -p "${s}${factor}" -s 1 -t 2 -l 32 -m 2   "$SCRIPT_PATH"/data/english.1024MB >>  bench/run$code/cmpr.data   
+done
+
+# bench mine vs given for different sizes;
+touch bench/run$code/stl.csv
+# echo "size,s,tau,leafsize,time,endmem,peakmem" >> bench/run$code/stl.csv
+# for s in $sB
+# do
+#     for t in 2 4 8 16 32 64
+#     do
+#         for l in 4 8 16 32 64
+#         do
+#             "$SCRIPT_PATH"/out/build/BlockTreesExperiments -p "10${factor}" -s "${s}" -t "${t}" -l "${l}" -m 0   "$SCRIPT_PATH"/data/data/dna.200MB >>  bench/run$code/stl.csv
+#         done
+#     done
+# done
